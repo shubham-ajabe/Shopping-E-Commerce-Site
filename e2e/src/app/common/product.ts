c1f88c5e277b2b6cb3148081f2100c66a007b5b6
Shubham Ajabe <66984159+shubham-ajabe@users.noreply.github.com>
2023-07-24 23:04:48 +0530

    Add files via upload

---
+++ e2e/src/app/common/product.ts
@@ -0,0 +1,13 @@
+export class Product{
+    "id":number;
+    "sku":string;
+    "name":string;
+    "description":string;
+    "imageUrl":string;
+    "unitPrice":number;
+    "active":boolean;
+    "unitInStocks":number;
+    "createdDate":Date;
+    "lastUpdate":Date;
+
+}
